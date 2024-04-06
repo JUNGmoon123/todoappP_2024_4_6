58227b5cd1f607b83d2201ed56ed1a724c4f705b
JUNGmoon123 <jungpro567@gmail.com>
2024-04-06 16:30:34 +0900

    tailwind, mui 충돌 피하는법

--- src/app/page.js
+++ src/app/page.js
@@ -1,12 +1,10 @@
 'use client';
-import * as React from 'react';
 import Button from '@mui/material/Button';
-import Image from 'next/image';
 
 export default function Home() {
   return (
     <>
-      <Button className="bg-red-500" variant="contained">
+      <Button className="tw-bg-red-500" variant="contained">
         Contained
       </Button>
     </>
